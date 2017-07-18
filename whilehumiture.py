8145ed621166b9f34edd1632cb0dec930205bdfb
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-07-18 14:50:18 -0700

    current timing and format

--- whilehumiture.py
+++ whilehumiture.py
@@ -9,7 +9,7 @@ import pytz
 while True:
     url = 'https://internet-of-stings.herokuapp.com/humiture'
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    
+
     today = datetime.datetime.now()
     pst = timezone('US/Pacific')
     today = pst.localize(today)
@@ -17,9 +17,10 @@ while True:
     date_format = '%m-%d-%y %H:%M:%S %Z'
 
     today_format = today.strftime(date_format)
-    
-    query = {'temperature': temperature, 'humidity': humidity, 'date_recorded': today_format, 'user_id': 1}
-    
-    if today.second % 2 == 0:
+
+    query = {'temperature': temperature, 'humidity': humidity, 'date_recorded': today, 'user_id': 1}
+
+    if today.minute % 10 == 0 and today.second > 5:
+    #if today.second % 5 ==0:
         res = requests.post(url, data=query)
         print('Temp: {0:0.1f} C., Humidity: {1:0.1f}%'.format(temperature, humidity), today_format)
